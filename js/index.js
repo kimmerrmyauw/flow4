f94fd12237fc9e04621de7d86f34c174b5caa43b
kimmerrmyauw <kimmyauwcanva@gmail.com>
2026-08-19 02:25:44 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Have Something`.split('')
+const text = `i know that people who come will go`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
